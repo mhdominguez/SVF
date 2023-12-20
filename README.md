0d68d38d2272d867be547b7344c1b632b90d7900
mhdominguez <martin.h.dominguez@gmail.com>
2023-12-20 11:19:01 -0500

    new file:   Blank Dataset.tar.gz 	modified:   README.md 	new file:   SVF2MaMuT.py 	new file:   config-files/svf2MM.csv 	new file:   mamut_xml_templates.py

--- README.md
+++ README.md
@@ -1,38 +1,56 @@
-# SVF
+# SVF and svf2MaMuT
 
-This repository contains an update to the Statistical Vector Flow (SVF) package and the tissue propagation software published in *In toto imaging and reconstruction of post-implantation mouse development at the single-cell level*.  Now compatible with Python 3.x, it has been updated for easier terminal/console use, it can handle complex path names, and there are minor bugs fixes.
+This repository contains the Statistical Vector Flow (SVF) package and the tissue propagation software published in *In toto imaging and reconstruction of post-implantation mouse development at the single-cell level*.  Now compatible with Python 3.x, it has been updated for easier terminal/console use, it can handle complex path names, is compatible with MaMuT v7, and there are other bugs fixes.  Note svf2MaMuT is now integrated into this SVF repository for easy setup.
 
 ## Description of the repository
 Folders:
   - IO: The class `SpatialImage`, a container for images and input/output. When the right external libraries are installed (see bellow), can read tiff, hdf5, klb and inr images.
   - TGMMlibraries: The class `lineageTree`, a container for lineage trees and Statistical Vector Flow (SVF). Can read output data from TGMM.
   - csv-parameter-files: Example of parameterization csv files for each algorithms.
-Python files:
-  - SVF-prop.py: python script to build Statistical Vector Flow from a TGMM dataset.
-  - tissue-bw-prop.py: python script to propagate tissue information from a manually annotated 3D image.
+Python files to be run IN ORDER:
+  - 1. SVF-prop.py: builds Statistical Vector Flow from a TGMM dataset.
+  - 2. tissue-bw-prop.py: propagates tissue information from a manually annotated 3D image.
+  - 3. SVF2MaMuT.py: exports results to MaMuT format for quantification and visualization.
 
 ## Basic usage
-Each of the python scripts proposed here can be run from a terminal in the following way:
+Each of the python scripts proposed here can be run from a terminal in the sequence:
 
-`python SVF-prop.py csv-parameter-files/SVF-prop.csv`
+`python3 /path/to/SVF/SVF-prop.py config-files/SVF-prop-config.txt`
 
-`python tissue-bw-prop.py csv-parameter-files/tissue-bw-prop.csv`
+`python3 /path/to/SVF/tissue-bw-prop.py config-files/tissue-bw-prop-config.txt`
 
-The user should modify the parameter files prior to running with the correct information.  Note, the first line of each .csv parameter file contains the field delimeter used for the rest of the file; in other words, the user can use a different delimeter than `###` or `'` if desired.
+`python3 /path/to/SVF/SVF2MaMuT.py config-files/svf2MM-config.txt`
+
+The user should modify the parameter files prior to running with the correct information.
 
 ## Dependencies
 Some dependecies are requiered:
   - general python dependecies:
     - numpy, scipy, pandas
   - SVF-prop.py:
-     - TGMMlibraries has to be installed (see TGMMlibraries README.md)
+     - TGMMlibraries installed (see TGMMlibraries README.md)
   - tissue-bw-prop.py:
-    - TGMMlibraries has to be installed (see TGMMlibraries README.md)
-    - IO library has to be installed (see IO README.md)
+    - TGMMlibraries installed (see TGMMlibraries README.md)
+    - IO library installed (see IO README.md)
+  - SVF2MaMuT.py:
+    - TGMMlibraries installed (see TGMMlibraries README.md)
+    - `Blank Dataset.tar.gz` contains an empty BigDataViewer dataset that is needed as a template.
 
 ## Quick install
-To quickly install the script so it can be call from the terminal and install too the common dependecies one can run
+Install IO and TGMMlibraries packages:
 ```shell
-python setup.py install [--user]
+cd ~/Downloads
+git clone https://github.com/GuignardLab/IO
+git clone https://github.com/leoguignard/TGMMlibraries
+cd IO
+sudo python3 setup.py install
+cd ../TGMMlibraries
+sudo python3 setup.py install
+cd ..
 ```
-Still will be remaining to install IO and TGMMlibraries packages.
+
+Install-free preparation of SVF:
+```shell
+git clone https://github.com/mhdominguez/SVF
+```
+
---
+++ SVF2MaMuT.py
@@ -0,0 +1,204 @@
+from TGMMlibraries import lineageTree
+import numpy as np
+import os
+from mamut_xml_templates import *
+import sys
+
+def read_param_file():
+    if (sys.argv[1] is not None) and (sys.argv[1][-4:] == '.csv'):
+        f_names = [sys.argv[1]]
+        #print f_names + "\n"
+    else:    
+        p_param = input('Please enter the path to the parameter file/folder:\n')
+        p_param = p_param.replace('"', '')
+        p_param = p_param.replace("'", '')
+        p_param = p_param.replace(" ", '')
+        if p_param[-4:] == '.csv':
+            f_names = [p_param]
+        else:
+            f_names = [os.path.join(p_param, f) for f in os.listdir(p_param) if '.csv' in f and not '~' in f]
+    for file_name in f_names:
+        f = open(file_name)
+        lines = f.readlines()
+        f.close()
+        param_dict = {}
+        i = 1
+        nb_lines = len(lines)
+        delimeter = lines[0]
+        delimeter = delimeter.rstrip()        
+        while i < nb_lines:
+            l = lines[i]
+            split_line = l.split(delimeter)
+            param_name = split_line[0]
+            if param_name in ['labels', 'downsampling', 'tissue_ids', 'tissue_names']:
+                name = param_name
+                out = []
+                while (name == param_name or param_name == '') and  i < nb_lines:
+                    if split_line[1].strip().isdigit():
+                        out += [int(split_line[1])]
+                    else:
+                        out += [(split_line[1].strip())]
+                    i += 1
+                    if i < nb_lines:
+                        l = lines[i]
+                        split_line = l.split(delimeter)
+                        param_name = split_line[0]
+                param_dict[name] = np.array(out)
+            elif param_name in ['label_names']:
+                name = param_name
+                out = []
+                while (name == param_name or param_name == '') and  i < nb_lines:
+                    out += [split_line[1].replace('\n', '').replace('\r', '')]
+                    i += 1
+                    if i < nb_lines:
+                        l = lines[i]
+                        split_line = l.split(delimeter)
+                        param_name = split_line[0]
+                param_dict[name] = np.array(out)
+            else:
+                param_dict[param_name] = split_line[1].strip()
+                i += 1
+            if param_name == 'time':
+                param_dict[param_name] = int(split_line[1])
+        path_SVF = param_dict.get('path_to_SVF', '.')
+        path_DB = param_dict.get('path_to_DB', '')
+        path_output = param_dict.get('path_output', '.')
+        path_LUT= param_dict.get('path_lut', '.')
+        #tissue_ids = param_dict.get('tissue_ids', [])
+        tissue_names = param_dict.get('tissue_names', [])
+        begin = int(param_dict.get('begin', None))
+        end = int(param_dict.get('end', None))
+        do_mercator = bool(int(param_dict.get('do_mercator', '0')))
+        filename = param_dict.get('filename', '.')
+        folder = param_dict.get('folder', '.')
+        v_size = np.float(param_dict.get('v_size', 0.))
+        dT = np.float(param_dict.get('dT', 1.))
+
+    return (path_SVF, path_DB, path_output, path_LUT,
+        tissue_names, begin, end, filename, folder, v_size, dT, do_mercator)
+
+def main():
+    try:
+        (path_SVF, path_to_DB, path_output, path_LUT,
+            tissue_names, begin, end, filename, folder, v_size, dT, do_mercator) = read_param_file()
+    except Exception as e:
+        print("Failed at reading the configuration file.")
+        print("Error: %s"%e)
+        raise e
+    
+    SVF = lineageTree(path_SVF)
+
+    # f = open(path_to_DB)
+    # lines = f.readline()
+    # f.close()
+    #print "Do mercator %s"%do_mercator
+    if os.path.exists(path_to_DB):
+        DATA = np.loadtxt(path_to_DB, delimiter = ',', skiprows = 1, usecols = (0, 6,7, 9))
+        tracking_value = dict(DATA[:, (0, 3)])
+        sphere_coord_theta = dict(DATA[:, (0, 1)])
+        sphere_coord_phi = dict(DATA[:, (0, 2)])
+        kept_nodes = [c for c in SVF.nodes if tracking_value[c] in tissue_ids]
+        kept_nodes_set = set(kept_nodes)
+        t_id_2_N = dict(list(zip(tissue_ids, tissue_names)))
+    else:
+        kept_nodes = SVF.nodes
+        kept_nodes_set = set(kept_nodes)
+        tracking_value = {c:1 for c in kept_nodes}
+
+
+    kept_times = list(range(begin, end+1))
+    first_nodes = [c for c in SVF.time_nodes[min(kept_times)] if c in kept_nodes_set]
+
+    if not os.path.exists(os.path.dirname(path_output)):
+        os.makedirs(os.path.dirname(path_output))
+    with open(path_output, 'w') as output:
+
+        output.write(begin_template)
+
+        # Begin AllSpots.
+        output.write(allspots_template.format(nspots=len(kept_nodes)))
+        
+        # Loop through lists of spots to try to center the model
+        Q = []
+        abs_center = [ 0.0,0.0,0.0 ]
+        if do_mercator:
+            for t in kept_times:
+               for c in SVF.time_nodes[t]:
+                   SVF.pos[c][0] = 20*np.arctan(np.exp(sphere_coord_theta[c]))-(np.pi/2 )
+                   SVF.pos[c][1] = 10*sphere_coord_phi[c]
+                   SVF.pos[c][2] = 0.0
+                   Q += [SVF.pos[c]]
+            abs_center = np.median(Q,axis=0)  
+        elif v_size > 0:
+            for t in kept_times:
+               for c in SVF.time_nodes[t]:
+                   SVF.pos[c][0] *= v_size
+                   SVF.pos[c][1] *= v_size
+                   SVF.pos[c][2] *= v_size
+        else:
+            for t in kept_times:
+               for c in SVF.time_nodes[t]:
+                   Q += [SVF.pos[c]]
+            abs_center = np.median(Q,axis=0) 
+            
+        # Loop through lists of spots.
+        for t in kept_times:
+            cells = kept_nodes_set.intersection(SVF.time_nodes.get(t, []))
+            if cells != []:
+                output.write(inframe_template.format(frame=t))
+                for c in cells:
+                    output.write(spot_template.format(id=c, name=c, frame=t, t_id=tracking_value[c],
+                                                      x=SVF.pos[c][0]-abs_center[0],
+                                                      y=SVF.pos[c][1]-abs_center[1],
+                                                      z=SVF.pos[c][2]-abs_center[2],
+                                                      t_name=t_id_2_N.get(tracking_value[c], '?')
+                                                      ))
+                output.write(inframe_end_template)
+            else:
+                output.write(inframe_empty_template.format(frame=t))
+
+
+        # End AllSpots.
+        output.write(allspots_end_template)
+
+        all_tracks = []
+        roots = set(kept_nodes).difference(SVF.predecessor).union(first_nodes)
+        last_time = max(kept_times)
+        for c in roots:
+            track = [c]
+            while track[-1] in SVF.successor and SVF.time[track[-1]]<last_time:
+                track += SVF.successor[track[-1]]
+            all_tracks += [track]
+
+        # Begin AllTracks.
+        output.write(alltracks_template)
+
+        for track_id, track in enumerate(all_tracks[::-1]):
+            stop = SVF.time[track[-1]]
+            duration = len(track)
+            output.write(track_template.format(id=track_id+1, duration=duration, 
+                                               start=SVF.time[track[0]], stop=stop, nspots=len(track),
+                                               displacement=np.linalg.norm(SVF.pos[track[0]]-SVF.pos[track[-1]])))
+            for c in track[:-1]:
+                displacement = np.linalg.norm(SVF.pos[c] - SVF.pos[SVF.successor[c][0]]) * v_size
+                velocity = displacement / dT
+                output.write(edge_template.format(source_id=c, target_id=SVF.successor[c][0],
+                                                  # t_name=t_id_2_N.get(tracking_value[c], '?'),
+                                                  velocity=velocity, displacement=displacement,
+                                                  t_id=tracking_value[c], time=SVF.time[c]))
+            output.write(track_end_template)
+
+        # End AllTracks.
+        output.write(alltracks_end_template)
+
+        # Filtered tracks.
+        output.write(filteredtracks_start_template)
+        for track_id, track in enumerate(all_tracks[::-1]):
+            output.write(filteredtracks_template.format(t_id=track_id+1))
+        output.write(filteredtracks_end_template)
+
+        # End XML file.
+        output.write(end_template.format(image_data=im_data_template.format(filename=filename, folder=folder)))
+
+if __name__ == '__main__':
+    main()
